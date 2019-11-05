26eed08ace313012c006cdb959d7d570bcc4d4f4
Ubuntu <ubuntu@ip-172-31-83-40.ec2.internal>
2019-11-05 21:19:29 +0000

    Changed id's on Index page to clearer description

--- assets/js/format.js
+++ assets/js/format.js
@@ -116,170 +116,170 @@ $(document).ready(function() {
     
     
     
-    // Show current bread advice
+    // Show current bread advice on index page
     function changeActiveDay() {
 
 
         if (activeDay == 2) {
             
-            $("#donkermeergranen_placed").val(AdviceThursday.donkermeergranen);
-            $("#zonnevolkoren_placed").val(AdviceThursday.zonnevolkoren);
-            $("#wit_placed").val(AdviceThursday.wit);
-            $("#tarwe_placed").val(AdviceThursday.tarwe);
-            $("#volkoren_placed").val(AdviceThursday.volkoren);
-            $("#mais_placed").val(AdviceThursday.mais);
-            $("#witmeerzaden_placed").val(AdviceThursday.witmeerzaden);
-            $("#spelt_placed").val(AdviceThursday.spelt);
-            $("#roggevijgen_placed").val(AdviceThursday.roggevijgen);
-            $("#roggerozijn_placed").val(AdviceThursday.roggerozijn);
-            $("#haverpompoen_placed").val(AdviceThursday.haverpompoen);
+            $("#donkermeergranen_tobe_placed").val(AdviceThursday.donkermeergranen);
+            $("#zonnevolkoren_tobe_placed").val(AdviceThursday.zonnevolkoren);
+            $("#wit_tobe_placed").val(AdviceThursday.wit);
+            $("#tarwe_tobe_placed").val(AdviceThursday.tarwe);
+            $("#volkoren_tobe_placed").val(AdviceThursday.volkoren);
+            $("#mais_tobe_placed").val(AdviceThursday.mais);
+            $("#witmeerzaden_tobe_placed").val(AdviceThursday.witmeerzaden);
+            $("#spelt_tobe_placed").val(AdviceThursday.spelt);
+            $("#roggevijgen_tobe_placed").val(AdviceThursday.roggevijgen);
+            $("#roggerozijn_tobe_placed").val(AdviceThursday.roggerozijn);
+            $("#haverpompoen_tobe_placed").val(AdviceThursday.haverpompoen);
             
-            $("#donkermeergranen_baked").val(AdviceTuesday.donkermeergranen);
-            $("#zonnevolkoren_baked").val(AdviceTuesday.zonnevolkoren);
-            $("#wit_baked").val(AdviceTuesday.wit);
-            $("#tarwe_baked").val(AdviceTuesday.tarwe);
-            $("#volkoren_baked").val(AdviceTuesday.volkoren);
-            $("#mais_baked").val(AdviceTuesday.mais);
-            $("#witmeerzaden_baked").val(AdviceTuesday.witmeerzaden);
-            $("#spelt_baked").val(AdviceTuesday.spelt);
-            $("#roggevijgen_baked").val(AdviceTuesday.roggevijgen);
-            $("#roggerozijn_baked").val(AdviceTuesday.roggerozijn);
-            $("#haverpompoen_baked").val(AdviceTuesday.haverpompoen);
+            $("#donkermeergranen_tobe_baked").val(AdviceTuesday.donkermeergranen);
+            $("#zonnevolkoren_tobe_baked").val(AdviceTuesday.zonnevolkoren);
+            $("#wit_tobe_baked").val(AdviceTuesday.wit);
+            $("#tarwe_tobe_baked").val(AdviceTuesday.tarwe);
+            $("#volkoren_tobe_baked").val(AdviceTuesday.volkoren);
+            $("#mais_tobe_baked").val(AdviceTuesday.mais);
+            $("#witmeerzaden_tobe_baked").val(AdviceTuesday.witmeerzaden);
+            $("#spelt_tobe_baked").val(AdviceTuesday.spelt);
+            $("#roggevijgen_tobe_baked").val(AdviceTuesday.roggevijgen);
+            $("#roggerozijn_tobe_baked").val(AdviceTuesday.roggerozijn);
+            $("#haverpompoen_tobe_baked").val(AdviceTuesday.haverpompoen);
             
         }
         if (activeDay == 3) {
             
-            $("#donkermeergranen_placed").val(AdviceFriday.donkermeergranen);
-            $("#zonnevolkoren_placed").val(AdviceFriday.zonnevolkoren);
-            $("#wit_placed").val(AdviceFriday.wit);
-            $("#tarwe_placed").val(AdviceFriday.tarwe);
-            $("#volkoren_placed").val(AdviceFriday.volkoren);
-            $("#mais_placed").val(AdviceFriday.mais);
-            $("#witmeerzaden_placed").val(AdviceFriday.witmeerzaden);
-            $("#spelt_placed").val(AdviceFriday.spelt);
-            $("#roggevijgen_placed").val(AdviceFriday.roggevijgen);
-            $("#roggerozijn_placed").val(AdviceFriday.roggerozijn);
-            $("#haverpompoen_placed").val(AdviceFriday.haverpompoen);
+            $("#donkermeergranen_tobe_placed").val(AdviceFriday.donkermeergranen);
+            $("#zonnevolkoren_tobe_placed").val(AdviceFriday.zonnevolkoren);
+            $("#wit_tobe_placed").val(AdviceFriday.wit);
+            $("#tarwe_tobe_placed").val(AdviceFriday.tarwe);
+            $("#volkoren_tobe_placed").val(AdviceFriday.volkoren);
+            $("#mais_tobe_placed").val(AdviceFriday.mais);
+            $("#witmeerzaden_tobe_placed").val(AdviceFriday.witmeerzaden);
+            $("#spelt_tobe_placed").val(AdviceFriday.spelt);
+            $("#roggevijgen_tobe_placed").val(AdviceFriday.roggevijgen);
+            $("#roggerozijn_tobe_placed").val(AdviceFriday.roggerozijn);
+            $("#haverpompoen_tobe_placed").val(AdviceFriday.haverpompoen);
             
-            $("#donkermeergranen_baked").val(AdviceWednesday.donkermeergranen);
-            $("#zonnevolkoren_baked").val(AdviceWednesday.zonnevolkoren);
-            $("#wit_baked").val(AdviceWednesday.wit);
-            $("#tarwe_baked").val(AdviceWednesday.tarwe);
-            $("#volkoren_baked").val(AdviceWednesday.volkoren);
-            $("#mais_baked").val(AdviceWednesday.mais);
-            $("#witmeerzaden_baked").val(AdviceWednesday.witmeerzaden);
-            $("#spelt_baked").val(AdviceWednesday.spelt);
-            $("#roggevijgen_baked").val(AdviceWednesday.roggevijgen);
-            $("#roggerozijn_baked").val(AdviceWednesday.roggerozijn);
-            $("#haverpompoen_baked").val(AdviceWednesday.haverpompoen);
+            $("#donkermeergranen_tobe_baked").val(AdviceWednesday.donkermeergranen);
+            $("#zonnevolkoren_tobe_baked").val(AdviceWednesday.zonnevolkoren);
+            $("#wit_tobe_baked").val(AdviceWednesday.wit);
+            $("#tarwe_tobe_baked").val(AdviceWednesday.tarwe);
+            $("#volkoren_tobe_baked").val(AdviceWednesday.volkoren);
+            $("#mais_tobe_baked").val(AdviceWednesday.mais);
+            $("#witmeerzaden_tobe_baked").val(AdviceWednesday.witmeerzaden);
+            $("#spelt_tobe_baked").val(AdviceWednesday.spelt);
+            $("#roggevijgen_tobe_baked").val(AdviceWednesday.roggevijgen);
+            $("#roggerozijn_tobe_baked").val(AdviceWednesday.roggerozijn);
+            $("#haverpompoen_tobe_baked").val(AdviceWednesday.haverpompoen);
             
         }
         if (activeDay == 4) {
             
-            $("#donkermeergranen_placed").val(AdviceSaturday.donkermeergranen);
-            $("#zonnevolkoren_placed").val(AdviceSaturday.zonnevolkoren);
-            $("#wit_placed").val(AdviceSaturday.wit);
-            $("#tarwe_placed").val(AdviceSaturday.tarwe);
-            $("#volkoren_placed").val(AdviceSaturday.volkoren);
-            $("#mais_placed").val(AdviceSaturday.mais);
-            $("#witmeerzaden_placed").val(AdviceSaturday.witmeerzaden);
-            $("#spelt_placed").val(AdviceSaturday.spelt);
-            $("#roggevijgen_placed").val(AdviceSaturday.roggevijgen);
-            $("#roggerozijn_placed").val(AdviceSaturday.roggerozijn);
-            $("#haverpompoen_placed").val(AdviceSaturday.haverpompoen);
+            $("#donkermeergranen_tobe_placed").val(AdviceSaturday.donkermeergranen);
+            $("#zonnevolkoren_tobe_placed").val(AdviceSaturday.zonnevolkoren);
+            $("#wittobe__placed").val(AdviceSaturday.wit);
+            $("#tarwe_tobe_placed").val(AdviceSaturday.tarwe);
+            $("#volkoren_tobe_placed").val(AdviceSaturday.volkoren);
+            $("#mais_tobe_placed").val(AdviceSaturday.mais);
+            $("#witmeerzaden_tobe_placed").val(AdviceSaturday.witmeerzaden);
+            $("#spelt_tobe_placed").val(AdviceSaturday.spelt);
+            $("#roggevijgen_tobe_placed").val(AdviceSaturday.roggevijgen);
+            $("#roggerozijn_tobe_placed").val(AdviceSaturday.roggerozijn);
+            $("#haverpompoen_tobe_placed").val(AdviceSaturday.haverpompoen);
             
-            $("#donkermeergranen_baked").val(AdviceThursday.donkermeergranen);
-            $("#zonnevolkoren_baked").val(AdviceThursday.zonnevolkoren);
-            $("#wit_baked").val(AdviceThursday.wit);
-            $("#tarwe_baked").val(AdviceThursday.tarwe);
-            $("#volkoren_baked").val(AdviceThursday.volkoren);
-            $("#mais_baked").val(AdviceThursday.mais);
-            $("#witmeerzaden_baked").val(AdviceThursday.witmeerzaden);
-            $("#spelt_baked").val(AdviceThursday.spelt);
-            $("#roggevijgen_baked").val(AdviceThursday.roggevijgen);
-            $("#roggerozijn_baked").val(AdviceThursday.roggerozijn);
-            $("#haverpompoen_baked").val(AdviceThursday.haverpompoen);
+            $("#donkermeergranen_tobe_baked").val(AdviceThursday.donkermeergranen);
+            $("#zonnevolkoren_tobe_baked").val(AdviceThursday.zonnevolkoren);
+            $("#wit_tobe_baked").val(AdviceThursday.wit);
+            $("#tarwe_tobe_baked").val(AdviceThursday.tarwe);
+            $("#volkoren_tobe_baked").val(AdviceThursday.volkoren);
+            $("#mais_tobe_baked").val(AdviceThursday.mais);
+            $("#witmeerzaden_tobe_baked").val(AdviceThursday.witmeerzaden);
+            $("#spelt_tobe_baked").val(AdviceThursday.spelt);
+            $("#roggevijgen_tobe_baked").val(AdviceThursday.roggevijgen);
+            $("#roggerozijn_tobe_baked").val(AdviceThursday.roggerozijn);
+            $("#haverpompoen_tobe_baked").val(AdviceThursday.haverpompoen);
             
         }
         if (activeDay == 5) {
             
-            $("#donkermeergranen_placed").val(AdviceSunday.donkermeergranen);
-            $("#zonnevolkoren_placed").val(AdviceSunday.zonnevolkoren);
-            $("#wit_placed").val(AdviceSunday.wit);
-            $("#tarwe_placed").val(AdviceSunday.tarwe);
-            $("#volkoren_placed").val(AdviceSunday.volkoren);
-            $("#mais_placed").val(AdviceSunday.mais);
-            $("#witmeerzaden_placed").val(AdviceSunday.witmeerzaden);
-            $("#spelt_placed").val(AdviceSunday.spelt);
-            $("#roggevijgen_placed").val(AdviceSunday.roggevijgen);
-            $("#roggerozijn_placed").val(AdviceSunday.roggerozijn);
-            $("#haverpompoen_placed").val(AdviceSunday.haverpompoen);
+            $("#donkermeergranen_tobe_placed").val(AdviceSunday.donkermeergranen);
+            $("#zonnevolkoren_tobe_placed").val(AdviceSunday.zonnevolkoren);
+            $("#wit_tobe_placed").val(AdviceSunday.wit);
+            $("#tarwe_tobe_placed").val(AdviceSunday.tarwe);
+            $("#volkoren_tobe_placed").val(AdviceSunday.volkoren);
+            $("#mais_tobe_placed").val(AdviceSunday.mais);
+            $("#witmeerzaden_tobe_placed").val(AdviceSunday.witmeerzaden);
+            $("#spelt_tobe_placed").val(AdviceSunday.spelt);
+            $("#roggevijgen_tobe_placed").val(AdviceSunday.roggevijgen);
+            $("#roggerozijn_tobe_placed").val(AdviceSunday.roggerozijn);
+            $("#haverpompoen_tobe_placed").val(AdviceSunday.haverpompoen);
             
-            $("#donkermeergranen_baked").val(AdviceFriday.donkermeergranen);
-            $("#zonnevolkoren_baked").val(AdviceFriday.zonnevolkoren);
-            $("#wit_baked").val(AdviceFriday.wit);
-            $("#tarwe_baked").val(AdviceFriday.tarwe);
-            $("#volkoren_baked").val(AdviceFriday.volkoren);
-            $("#mais_baked").val(AdviceFriday.mais);
-            $("#witmeerzaden_baked").val(AdviceFriday.witmeerzaden);
-            $("#spelt_baked").val(AdviceFriday.spelt);
-            $("#roggevijgen_baked").val(AdviceFriday.roggevijgen);
-            $("#roggerozijn_baked").val(AdviceFriday.roggerozijn);
-            $("#haverpompoen_baked").val(AdviceFriday.haverpompoen);
+            $("#donkermeergranen_tobe_baked").val(AdviceFriday.donkermeergranen);
+            $("#zonnevolkoren_tobe_baked").val(AdviceFriday.zonnevolkoren);
+            $("#wit_tobe_baked").val(AdviceFriday.wit);
+            $("#tarwe_tobe_baked").val(AdviceFriday.tarwe);
+            $("#volkoren_tobe_baked").val(AdviceFriday.volkoren);
+            $("#mais_tobe_baked").val(AdviceFriday.mais);
+            $("#witmeerzaden_tobe_baked").val(AdviceFriday.witmeerzaden);
+            $("#spelt_tobe_baked").val(AdviceFriday.spelt);
+            $("#roggevijgen_tobe_baked").val(AdviceFriday.roggevijgen);
+            $("#roggerozijn_tobe_baked").val(AdviceFriday.roggerozijn);
+            $("#haverpompoen_tobe_baked").val(AdviceFriday.haverpompoen);
             
         }
         if (activeDay == 6) {
             
-            $("#donkermeergranen_placed").val(AdviceTuesday.donkermeergranen);
-            $("#zonnevolkoren_placed").val(AdviceTuesday.zonnevolkoren);
-            $("#wit_placed").val(AdviceTuesday.wit);
-            $("#tarwe_placed").val(AdviceTuesday.tarwe);
-            $("#volkoren_placed").val(AdviceTuesday.volkoren);
-            $("#mais_placed").val(AdviceTuesday.mais);
-            $("#witmeerzaden_placed").val(AdviceTuesday.witmeerzaden);
-            $("#spelt_placed").val(AdviceTuesday.spelt);
-            $("#roggevijgen_placed").val(AdviceTuesday.roggevijgen);
-            $("#roggerozijn_placed").val(AdviceTuesday.roggerozijn);
-            $("#haverpompoen_placed").val(AdviceTuesday.haverpompoen);
+            $("#donkermeergranen_tobe_placed").val(AdviceTuesday.donkermeergranen);
+            $("#zonnevolkoren_tobe_placed").val(AdviceTuesday.zonnevolkoren);
+            $("#wit_tobe_placed").val(AdviceTuesday.wit);
+            $("#tarwe_tobe_placed").val(AdviceTuesday.tarwe);
+            $("#volkoren_tobe_placed").val(AdviceTuesday.volkoren);
+            $("#mais_tobe_placed").val(AdviceTuesday.mais);
+            $("#witmeerzaden_tobe_placed").val(AdviceTuesday.witmeerzaden);
+            $("#spelt_tobe_placed").val(AdviceTuesday.spelt);
+            $("#roggevijgen_tobe_placed").val(AdviceTuesday.roggevijgen);
+            $("#roggerozijn_tobe_placed").val(AdviceTuesday.roggerozijn);
+            $("#haverpompoen_tobe_placed").val(AdviceTuesday.haverpompoen);
             
-            $("#donkermeergranen_baked").val(AdviceSaturday.donkermeergranen);
-            $("#zonnevolkoren_baked").val(AdviceSaturday.zonnevolkoren);
-            $("#wit_baked").val(AdviceSaturday.wit);
-            $("#tarwe_baked").val(AdviceSaturday.tarwe);
-            $("#volkoren_baked").val(AdviceSaturday.volkoren);
-            $("#mais_baked").val(AdviceSaturday.mais);
-            $("#witmeerzaden_baked").val(AdviceSaturday.witmeerzaden);
-            $("#spelt_baked").val(AdviceSaturday.spelt);
-            $("#roggevijgen_baked").val(AdviceSaturday.roggevijgen);
-            $("#roggerozijn_baked").val(AdviceSaturday.roggerozijn);
-            $("#haverpompoen_baked").val(AdviceSaturday.haverpompoen);
+            $("#donkermeergranen_tobe_baked").val(AdviceSaturday.donkermeergranen);
+            $("#zonnevolkoren_tobe_baked").val(AdviceSaturday.zonnevolkoren);
+            $("#wit_tobe_baked").val(AdviceSaturday.wit);
+            $("#tarwe_tobe_baked").val(AdviceSaturday.tarwe);
+            $("#volkoren_tobe_baked").val(AdviceSaturday.volkoren);
+            $("#mais_tobe_baked").val(AdviceSaturday.mais);
+            $("#witmeerzaden_tobe_baked").val(AdviceSaturday.witmeerzaden);
+            $("#spelt_tobe_baked").val(AdviceSaturday.spelt);
+            $("#roggevijgen_tobe_baked").val(AdviceSaturday.roggevijgen);
+            $("#roggerozijn_tobe_baked").val(AdviceSaturday.roggerozijn);
+            $("#haverpompoen_tobe_baked").val(AdviceSaturday.haverpompoen);
             
         }
         if (activeDay == 0) {
             
-            $("#donkermeergranen_placed").val(AdviceWednesday.donkermeergranen);
-            $("#zonnevolkoren_placed").val(AdviceWednesday.zonnevolkoren);
-            $("#wit_placed").val(AdviceWednesday.wit);
-            $("#tarwe_placed").val(AdviceWednesday.tarwe);
-            $("#volkoren_placed").val(AdviceWednesday.volkoren);
-            $("#mais_placed").val(AdviceWednesday.mais);
-            $("#witmeerzaden_placed").val(AdviceWednesday.witmeerzaden);
-            $("#spelt_placed").val(AdviceWednesday.spelt);
-            $("#roggevijgen_placed").val(AdviceWednesday.roggevijgen);
-            $("#roggerozijn_placed").val(AdviceWednesday.roggerozijn);
-            $("#haverpompoen_placed").val(AdviceWednesday.haverpompoen);
+            $("#donkermeergranen_tobe_placed").val(AdviceWednesday.donkermeergranen);
+            $("#zonnevolkoren_tobe_placed").val(AdviceWednesday.zonnevolkoren);
+            $("#wit_tobe_placed").val(AdviceWednesday.wit);
+            $("#tarwe_tobe_placed").val(AdviceWednesday.tarwe);
+            $("#volkoren_tobe_placed").val(AdviceWednesday.volkoren);
+            $("#mais_tobe_placed").val(AdviceWednesday.mais);
+            $("#witmeerzaden_tobe_placed").val(AdviceWednesday.witmeerzaden);
+            $("#spelt_tobe_placed").val(AdviceWednesday.spelt);
+            $("#roggevijgen_tobe_placed").val(AdviceWednesday.roggevijgen);
+            $("#roggerozijn_tobe_placed").val(AdviceWednesday.roggerozijn);
+            $("#haverpompoen_tobe_placed").val(AdviceWednesday.haverpompoen);
             
-            $("#donkermeergranen_baked").val(AdviceSunday.donkermeergranen);
-            $("#zonnevolkoren_baked").val(AdviceSunday.zonnevolkoren);
-            $("#wit_baked").val(AdviceSunday.wit);
-            $("#tarwe_baked").val(AdviceSunday.tarwe);
-            $("#volkoren_baked").val(AdviceSunday.volkoren);
-            $("#mais_baked").val(AdviceSunday.mais);
-            $("#witmeerzaden_baked").val(AdviceSunday.witmeerzaden);
-            $("#spelt_baked").val(AdviceSunday.spelt);
-            $("#roggevijgen_baked").val(AdviceSunday.roggevijgen);
-            $("#roggerozijn_baked").val(AdviceSunday.roggerozijn);
-            $("#haverpompoen_baked").val(AdviceSunday.haverpompoen);
+            $("#donkermeergranen_tobe_baked").val(AdviceSunday.donkermeergranen);
+            $("#zonnevolkoren_tobe_baked").val(AdviceSunday.zonnevolkoren);
+            $("#wit_tobe_baked").val(AdviceSunday.wit);
+            $("#tarwe_tobe_baked").val(AdviceSunday.tarwe);
+            $("#volkoren_tobe_baked").val(AdviceSunday.volkoren);
+            $("#mais_tobe_baked").val(AdviceSunday.mais);
+            $("#witmeerzaden_tobe_baked").val(AdviceSunday.witmeerzaden);
+            $("#spelt_tobe_baked").val(AdviceSunday.spelt);
+            $("#roggevijgen_tobe_baked").val(AdviceSunday.roggevijgen);
+            $("#roggerozijn_tobe_baked").val(AdviceSunday.roggerozijn);
+            $("#haverpompoen_tobe_baked").val(AdviceSunday.haverpompoen);
             
         }
 
